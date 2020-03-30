82d955f3a0f5073c9c8cad306d889b22b13835a2
Cristiano <valentisenior@gmail.com>
2020-03-30 18:23:10 +0200

    Non funziona l'if ex1

---
+++ main.js
@@ -0,0 +1,34 @@
+// Chiedo a utente di inserire come input una parola
+// tramite una funzione (creata da te) verifico se la parola inserita è palindroma o meno;
+// dò output a utente dicendogli che ho verificato;
+// qui sicuramente da cercare ci sarà il come revertare una parola.
+
+var parolaUtente = document.getElementById("parola");
+
+var bottoneEsegui = document.getElementById("esegui");
+
+bottoneEsegui.addEventListener("click",
+function(){
+// leggo la parola
+var parola = parolaUtente.value;
+console.log(parola);
+var nuova = parola.split("");
+console.log(nuova);
+//giro la parola (cercare)
+nuova= nuova.reverse();
+console.log(nuova);
+// la riunisco
+nuova= nuova.join();
+console.log(nuova);
+
+// var palindromaForse = "palindroma";
+// paragonare le due parole
+// se sono uguali ...
+if ( nuova == parola ) {
+  console.log("palindroma");
+    // se sono diverse
+}else {
+  console.log("non palindroma");
+}
+}
+)
